6f14b03b95820e9cbc36028be633144e5a603cd7
sathish chinniah <sathish73ec@gmail.com>
2015-10-26 23:08:29 +0530

    init

---
+++ ProgressViewDemo/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  ProgressViewDemo
+//
+//  Created by Sathish Chinniah on 26/10/15.
+//  Copyright © 2015 Sathish Chinniah. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ ProgressViewDemo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ProgressViewDemo
+//
+//  Created by Sathish Chinniah on 26/10/15.
+//  Copyright © 2015 Sathish Chinniah. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
